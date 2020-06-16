395b7a036985f954bdc0fdbba54284c4f57d7fb7
Edwin Harmata <EHarmata>
2020-06-16 11:14:31 +0200

    Updates

---
+++ precache-manifest.6eedad55316825a81f1b2b4d3150c6f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8493ae5bc8209a6897ebeb069e103f0e",
+    "url": "/persony-engine/index.html"
+  },
+  {
+    "revision": "68f14cc7ec27c7ec12d6",
+    "url": "/persony-engine/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "536a9b14b862ace7250c",
+    "url": "/persony-engine/static/css/main.7492db9e.chunk.css"
+  },
+  {
+    "revision": "68f14cc7ec27c7ec12d6",
+    "url": "/persony-engine/static/js/2.2ef3d903.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/persony-engine/static/js/2.2ef3d903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536a9b14b862ace7250c",
+    "url": "/persony-engine/static/js/main.33d58c04.chunk.js"
+  },
+  {
+    "revision": "7739f36adc51828762ee",
+    "url": "/persony-engine/static/js/runtime-main.a196579b.js"
+  }
+]);
